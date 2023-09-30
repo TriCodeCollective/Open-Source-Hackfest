c709f1f24fd38da3a1beb920ee9de34b53748cd1
keetha805 <Ketha508@gmail.com>
2023-10-01 00:58:11 +0200

    Adding home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,37 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <div class="flex flex-col h-screen justify-center items-center">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-dark">
+          Welcome to TriCodeCollective!
+        </h1>
+        <p class=" text-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+          Here at TriCodeCollective team we focus on bringing articles, videos,
+          open source resources, and much more; in order to make much easier for
+          you to learn!
+        </p>
+        <a
+          href="/about"
+          class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        >
+          Learn more
+          <svg
+            class="w-3.5 h-3.5 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
     </main>
   );
 }
